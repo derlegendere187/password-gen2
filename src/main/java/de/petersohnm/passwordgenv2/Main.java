f5f0a1efa47e5a8f3e05c5c72766a605ace00e6b
mateusz petersohn <mateusz.petersohn@procilon.de>
2026-01-29 15:19:31 +0100

    added: no args given, printHelpMenu() will open

--- src/main/java/de/petersohnm/passwordgenv2/Main.java
+++ src/main/java/de/petersohnm/passwordgenv2/Main.java
@@ -2,6 +2,8 @@ package de.petersohnm.passwordgenv2;
 
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,9 +12,13 @@ public class Main {
         boolean uppercaseAllowed = true;    // -u
         boolean numbersAllowed = true;      // -n
 
-        searchHelp(args);
+        if (args.length == 0) {
+            printHelpMenu();
+            System.exit(0);
+        }
 
-        lengthCheck(args);
+        searchHelp(args);
+        pwlength = lengthCheck(args);
 
         for (String arg : args) {
 
@@ -28,9 +34,15 @@ public class Main {
                 numbersAllowed = false;
             }
         }
+        System.out.println("---DEBUG AREA---");
+        System.out.println(pwlength);
+        System.out.println(specialcharacters);
+        System.out.println(uppercaseAllowed);
+        System.out.println(numbersAllowed);
+        System.out.println("----------------");
     }
 
-    public static void lengthCheck(String[] args) {
+    public static int lengthCheck(String[] args) {
         int pwlength = 0;
         boolean lengthIsNotValid = true;
 
@@ -39,11 +51,11 @@ public class Main {
                 try {
                     pwlength = Integer.parseInt(args[pos + 1]);
                     if (pwlength <= 0) {
-                        System.out.println("Die Länge des Passwortes muss mindestens eine größe von 1 haben.");
+                        System.out.println("The password length must be at least 1.");
                         System.exit(0);
                     }
                 } catch (Exception e) {
-                    System.out.println("Bitte gib eine gültige Ganzzahl für das Passwort ein!");
+                    System.out.println("Please enter a valid integer for the password length.");
                 }
                 lengthIsNotValid = false;
             }
@@ -51,13 +63,14 @@ public class Main {
         if (lengthIsNotValid) {
             System.exit(0);
         }
+        return pwlength;
     }
 
     public static void searchHelp(String[] args) {
         String[] helpSymbols = new String[]{"--help", "-h", "?", "-?", "/?"};
 
         for (String arg : args) {
-            if (Arrays.stream(helpSymbols).toList().contains(arg) || arg.isBlank()) {
+            if (Arrays.stream(helpSymbols).toList().contains(arg) || arg.isEmpty()) {
                 printHelpMenu();
                 break;
             } else {
@@ -89,5 +102,4 @@ public class Main {
     }
 }
 
-// TODO: default no command found -> help
-// TODO: boolean umdrehen, wenn gesetzt wurde
+// TODO: no command found -> help
