9c9bcff0cf58d4198bb78c46c1ceb6e278471b16
mateusz petersohn <mateusz.petersohn@procilon.de>
2026-01-30 08:03:01 +0100

    added some docs and changes helpMenu

--- src/main/java/de/petersohnm/passwordgenv2/Main.java
+++ src/main/java/de/petersohnm/passwordgenv2/Main.java
@@ -7,6 +7,7 @@ import java.util.random.RandomGenerator;
 
 public class Main {
     public static void main(String[] args) {
+        String[] allowedSymbols = new String[]{"-l", "-s", "-u", "-n", "--help", "-h", "?", "-?", "/?"};
         int pwlength = 0;                   // -l
         boolean specialcharacters = true;   // -s
         boolean uppercaseAllowed = true;    // -u
@@ -35,13 +36,18 @@ public class Main {
             }
         }
         System.out.println("---DEBUG AREA---");
-        System.out.println(pwlength);
-        System.out.println(specialcharacters);
-        System.out.println(uppercaseAllowed);
-        System.out.println(numbersAllowed);
+        System.out.println("length:" + pwlength);
+        System.out.println("special chars:" + specialcharacters);
+        System.out.println("uppercases:" + uppercaseAllowed);
+        System.out.println("numbers" + numbersAllowed);
         System.out.println("----------------");
     }
 
+    /**
+     * Checks everything, that has to do with the number input (integer, length, ...)
+     * @param args The arguments provided by user input.
+     * @return The checked password length.
+     */
     public static int lengthCheck(String[] args) {
         int pwlength = 0;
         boolean lengthIsNotValid = true;
@@ -66,10 +72,15 @@ public class Main {
         return pwlength;
     }
 
+    /**
+     * Checks if any of the help symbols are used.
+     * @param args The arguments provided by user input.
+     */
     public static void searchHelp(String[] args) {
         String[] helpSymbols = new String[]{"--help", "-h", "?", "-?", "/?"};
 
         for (String arg : args) {
+            // Prints the help menu, if no args found
             if (Arrays.stream(helpSymbols).toList().contains(arg) || arg.isEmpty()) {
                 printHelpMenu();
                 break;
@@ -79,6 +90,9 @@ public class Main {
         }
     }
 
+    /**
+     * Prints the help menu.
+     */
     public static void printHelpMenu() {
         String helptext =
                 """
@@ -86,15 +100,15 @@ public class Main {
                     <> - necessary
                     [] - optional
                 
-                    usage: Main.java <-l <int>>
-                                     [-s <boolean>]
-                                     [-u <boolean>]
-                                     [-n <boolean>]
+                    usage: Main.java <-l <int>>         // password length
+                                     [-s]               // special characters
+                                     [-u]               // uppercases
+                                     [-n]               // numbers
                 
                     explanation: '-l'   Sets the length of the password.
-                                 '-s'   If true, special characters will be used in the password.
-                                 '-u'   If true, uppercase letters will be used in the password.
-                                 '-n'   If true, numbers will be used in the password.
+                                 '-s'   When used, special characters will NOT be implemented in the password.
+                                 '-u'   When used, uppercase letters will NOT be used in the password.
+                                 '-n'   When used, numbers will NOT be used in the password.
                 
                     '--help' or '-h' to see helpful information.
                 """;
@@ -103,3 +117,4 @@ public class Main {
 }
 
 // TODO: no command found -> help
+//
