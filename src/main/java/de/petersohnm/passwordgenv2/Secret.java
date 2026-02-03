e3757730fa946beb0ec8194e1269ec3a1e04bc70
mateusz petersohn <mateusz.petersohn@procilon.de>
2026-02-03 16:13:03 +0100

    added checkInvalidChacter()

---
+++ src/main/java/de/petersohnm/passwordgenv2/Secret.java
@@ -0,0 +1,9 @@
+package de.petersohnm.passwordgenv2;
+
+public class Secret {
+
+    public static void secret() {
+        System.out.println("Oha, du hast ein tolles Secret gefunden.");
+        System.out.println("Dafür bekommst du nichts.");
+    }
+}
