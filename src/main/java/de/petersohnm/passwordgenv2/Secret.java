3866c2011e845e24d7f0d2284658f04519034e04
mateusz petersohn <mateusz.petersohn@procilon.de>
2026-02-05 11:14:01 +0100

    version 1.3.3A

--- src/main/java/de/petersohnm/passwordgenv2/Secret.java
+++ src/main/java/de/petersohnm/passwordgenv2/Secret.java
@@ -1,9 +1,16 @@
 package de.petersohnm.passwordgenv2;
 
+import java.util.concurrent.TimeUnit;
+
 public class Secret {
 
     public static void secret() {
         System.out.println("Oha, du hast ein tolles Secret gefunden.");
         System.out.println("Dafür bekommst du nichts.");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Spaß, hier hast du was:");
+        System.out.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        System.out.println("Viel Spaß damit!");
+        System.exit(0);
     }
 }
