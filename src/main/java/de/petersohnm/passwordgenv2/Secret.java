97ff1ec6bb124c23cd731db4a73a4f5732c5d463
mateusz petersohn <mateusz.petersohn@procilon.de>
2026-02-05 11:19:14 +0100

    version 1.3.3B

--- src/main/java/de/petersohnm/passwordgenv2/Secret.java
+++ src/main/java/de/petersohnm/passwordgenv2/Secret.java
@@ -4,10 +4,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Secret {
 
-    public static void secret() {
+    public static void secret() throws InterruptedException {
         System.out.println("Oha, du hast ein tolles Secret gefunden.");
         System.out.println("Dafür bekommst du nichts.");
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(2);
         System.out.println("Spaß, hier hast du was:");
         System.out.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
         System.out.println("Viel Spaß damit!");
